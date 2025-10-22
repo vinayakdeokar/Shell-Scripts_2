a1ef1ac88ef356f6cad1c0a1cb30857f94284e82
vinayakdeokar <vinayakdeokar121@gmail.com>
2025-10-22 19:22:11 +0530

    added all shell scripts version_2

--- 15_list_dir_tree.sh
+++ 15_list_dir_tree.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 #######################################
 # Script Name   : find_old_files.sh
-# Version       : 1.0
+# Version       : 2.0
 # Date          : 22/10/2025
 # Author        : Vinayak Deokar
 # Purpose       : Finds files not accessed in specified number of days
 #######################################
+DIR=$1
+DAYS=$2
 
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <directory> <days>"
     echo "Example: $0 /home/user 30"
     exit 1
 fi
-
-DIR=$1
-DAYS=$2
-
 if [ ! -d "$DIR" ]; then
     echo "Error: Directory $DIR does not exist."
     exit 1
