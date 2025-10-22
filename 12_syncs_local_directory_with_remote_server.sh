56b83487f2a21bd04f58a45b9493c61a61faffcd
vinayakdeokar <vinayakdeokar121@gmail.com>
2025-10-22 19:21:25 +0530

    added all shell scripts version_2

--- 12_syncs_local_directory_with_remote_server.sh
+++ 12_syncs_local_directory_with_remote_server.sh
@@ -6,17 +6,15 @@
 # Author        : Vinayak Deokar
 # Purpose       : Periodically sync a local directory with a remote server using rsync
 ###############################################
-
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <local_dir> <remote_user> <remote_host> <remote_dir>"
-    exit 1
-fi
-
 LOCAL_DIR=$1
 REMOTE_USER=$2
 REMOTE_HOST=$3
 REMOTE_DIR=$4
 
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <local_dir> <remote_user> <remote_host> <remote_dir>"
+    exit 1
+fi
 if [ ! -d "$LOCAL_DIR" ]; then
     echo "Error: Local directory '$LOCAL_DIR' not found."
     exit 1
